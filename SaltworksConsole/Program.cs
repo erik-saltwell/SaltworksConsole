bcc992aed4a3de9bac0282bf8b141ec7d9c80df6
erik-saltwell <169001579+erik-saltwell@users.noreply.github.com>
2024-08-30 15:59:18 -0700

    first push

---
+++ SaltworksConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using Autofac.Core;
+using Autofac;
+using Serilog;
+using Serilog.Extensions.Autofac.DependencyInjection;
+using Serilog.Configuration;
+using CommandLine;
+
+
+namespace SaltworksConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create your builder.
+            ContainerBuilder builder = new ContainerBuilder();
+            LoggerConfiguration log_configuration = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.Console();
+            builder.RegisterSerilog(log_configuration);
+
+            IContainer service = builder.Build();
+
+
+            Parser.Default.ParseArguments<CommandLineArgurments>(args).WithParsed<CommandLineArgurments>(arguments =>
+            {
+                //do work here
+            });
+        }
+    }
+}
